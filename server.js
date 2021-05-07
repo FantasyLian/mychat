0be957bc788a4a400632e33be5910d59eeaf0f23
Mr.cat <fantasy.lian@163.com>
2021-05-07 18:26:59 +0800

    修改服务器设置

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-/*
- * @Author: Mr.cat
- * @Date: 2021-04-26 18:03:57
- * @LastEditTime: 2021-04-27 13:58:50
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /mychat/server.js
- */
-const http = require('http')
-const Koa = require('koa')
-const io = require('socket.io')
-const uuid = require('uuid-v4')
-const server = new Koa()
+const Koa = require("koa")
+const app = new Koa()
+const server = require("http").Server(app.callback())
+const ws = require("socket.io")(server)
+const uuid = require('uuid/v4')
 
-const httpServer = http.createServer(server.callback())
-httpServer.listen(8585)
-
-const wsServer = io.listen(httpServer)
-
-wsServer.on('connection', sock => {
+ws.on("connection", socket => {
   console.log('connected')
-
   const ID = uuid()
-  sock.emit('ID', ID)
 
-  sock.on('msg', (user, msg) => {
-    wsServer.emit('broadcast', ID, user, msg)
+  socket.emit('ID', ID)
+
+  socket.on('msg', (user, msg) => {
+    ws.emit('broadcast', ID, user, msg)
   })
+})
+
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*")
+  await next()
+})
+
+server.listen(8080, () => {
+  console.log("ws listening on 8080")
 })
